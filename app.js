b9d419ccf7b11163185c7227f18d11d547882d94
alexzandros <carlosroberto.rojas@gmail.com>
2015-07-05 00:49:22 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,31 @@
+(function () {
+    var app = angular.module('mercado', []);
+    app.controller('mercadoController', function ($scope) {
+        $scope.filaEditable = -1;
+        $scope.listaDeCompras=[];
+        $scope.linea={};
+        $scope.agregarArticulo = function(){
+            $scope.listaDeCompras.push(angular.copy($scope.linea));
+            $scope.linea={};
+        };
+        $scope.totalizar = function () {
+            total = 0;
+            angular.forEach($scope.listaDeCompras,function(value,key){
+                total += value.cantidad * value.precio;
+            });
+            return total;
+        };
+        $scope.borrar = function (nombre) {
+            if(confirm("¿Realmente desea borrar " + nombre + "?"))
+            {
+                pos = $scope.listaDeCompras.map(function(elemento){
+                return elemento.articulo;    
+                }).indexOf(nombre);
+                $scope.listaDeCompras.splice(pos,1);    
+            }
+        };
+        $scope.setFilaEditable = function (fila) {
+            $scope.filaEditable = fila;   
+        }
+    });
+})();
